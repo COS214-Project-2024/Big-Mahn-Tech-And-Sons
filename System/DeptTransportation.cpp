9c2c58f89d3a963b5a0d2ef6692e22072d80428a
David <u23534975@tuks.co.za>
2024-10-28 19:06:13 +0200

    Created DeptTransportation.cpp

---
+++ System/DeptTransportation.cpp
@@ -0,0 +1,14 @@
+#include "DeptTransportation.h"
+
+DeptTransportation::DeptTransportation()
+{
+    
+}
+
+DeptTransportation::~DeptTransportation()
+{
+
+}
+
+
+// add the other ones in the header file
