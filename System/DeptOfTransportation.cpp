f6d3c1c5723d2f62d01681c9a10261ced801ebe8
David <u23534975@tuks.co.za>
2024-10-30 11:18:09 +0200

    You can now interact with Det of Trans

--- System/DeptOfTransportation.cpp
+++ System/DeptOfTransportation.cpp
@@ -21,23 +21,71 @@ DeptOfTransportation* DeptOfTransportation::getInstance()
     return instance;
 }
 
-bool DeptOfTransportation::addRoad(int start_row, int start_col, int street_Length, string direction, string street_name)
+bool DeptOfTransportation::add_Road(int start_row, int start_col, int street_Length, string direction, string street_name)
 {
     if(cityGrid->addRoad(start_row,start_col,street_Length,direction,street_name))
     {
+        cout<<"\nRoad added ~ DeptTrans\n";
         return true;
     }
-    cout<<"Road not added ~ DeptTrans";
+    cout<<"\nRoad not added ~ DeptTrans\n";
     return false;
 }
 
 void DeptOfTransportation::printCityGrid()
 {
-
+    cityGrid->printCityGrid();
 }
 
-void DeptOfTransportation::printRoadGrid()
+void DeptOfTransportation::printRoadNetwork()
 {
-
+    cityGrid->printCityRoadNetwork();
 }
 
+void DeptOfTransportation::printCityCardinalPoints()
+{
+    cityGrid->printCityCardinal();
+}
+
+void DeptOfTransportation::printCityStreets()
+{
+    cityGrid->printCityStreets();
+}
+
+bool DeptOfTransportation::remove_Road(string streetName)
+{
+    if(cityGrid->removeRoad(streetName))
+    {
+        cout<<"\nRoad removed ~ DeptTrans\n";
+        return true;
+    }
+
+    cout<<"\nRoad not removed ~ DeptTrans\n";
+    return false;
+}
+
+bool DeptOfTransportation::remove_Road(int start_row, int start_col, int road_length, string direction)
+{
+    if(cityGrid->removeRoad(start_row,start_col,road_length,direction))
+    {
+        cout<<"\nRoad removed ~ DeptTrans\n";
+        return true;
+    }
+    
+    cout<<"\nRoad not removed ~ DeptTrans\n";
+    return false;
+}
+
+const vector<pair<int,int>> DeptOfTransportation::add_Building(int length, int width, string building_type)
+{
+    vector<pair<int,int>> error = {{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+    vector<pair<int,int>> result = cityGrid->addBuilding(length,width,building_type);
+    if(result==error)
+    {
+        cout<<"\nBuilding not added to grid ~DeptTrans.\n";
+        return result;
+    }
+
+    cout<<"\nBuilding added to grid ~ DeptTrans.\n";
+    return result;
+}
--- System/DeptOfTransportation.h
+++ System/DeptOfTransportation.h
@@ -14,9 +14,22 @@ private:
 public:
     static DeptOfTransportation* getInstance();
     ~DeptOfTransportation();  //destructs the department object
+
+
+    //....... PRINT FUNCTIONS ........//
+
     void printCityGrid();   //prints the city grid, roads and buildings included
-    void printRoadGrid();   //prints the road grid, roads only
-    bool addRoad(int,int,int,string,string);
+    void printRoadNetwork();   //prints the road grid, roads only
+    void printCityCardinalPoints();
+    void printCityStreets();
+
+    // ......... ROAD AND BUILDING FUNCTIONS .......... //
+
+
+    bool add_Road(int,int,int,string,string);
+    bool remove_Road(int,int,int,string);
+    bool remove_Road(string);
+    const vector<pair<int,int>> add_Building(int,int,string);
     
 };
 
